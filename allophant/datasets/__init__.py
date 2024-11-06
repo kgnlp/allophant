c0f5c95eea96b813f04e1c185518a828e637aeb0
Mohammadmohsen Goodarzi <mohammadmohsen.goodarzi@idmt.fraunhofer.de>
2024-11-06 17:13:45 +0100

    Remove speech datasets

--- allophant/datasets/__init__.py
+++ allophant/datasets/__init__.py
@@ -1,13 +1,13 @@
 from allophant.datasets.speech_corpus import MultilingualCorpus, MultilingualSplits, AudioInfo
-from allophant.datasets.ucla_phonetic_corpus import UCLAPhoneticCorpus
-from allophant.datasets.mozilla_common_voice import CommonVoiceCorpus, CommonVoiceSplits
+#from allophant.datasets.ucla_phonetic_corpus import UCLAPhoneticCorpus
+#from allophant.datasets.mozilla_common_voice import CommonVoiceCorpus, CommonVoiceSplits
 
 
 __all__ = [
     "MultilingualCorpus",
     "MultilingualSplits",
     "AudioInfo",
-    "UCLAPhoneticCorpus",
-    "CommonVoiceSplits",
-    "CommonVoiceCorpus",
+#    "UCLAPhoneticCorpus",
+#    "CommonVoiceSplits",
+#    "CommonVoiceCorpus",
 ]
